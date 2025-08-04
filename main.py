8bbae7b2dadf60baa351983d0aa697f8de6ace48
halitklyc <halitenes.kalayci@gmail.com>
2025-08-04 13:57:32 +0300

    scope & for & if

--- main.py
+++ main.py
@@ -80,3 +80,52 @@ print(ogrenci == True or yas >= 20) # or -> iki koşuldan bir tanesinin true olm
 # and her zaman önceliklidir (parantez yoksa)
 print(True and True or False and True)
 #
+
+# Scope Kavramı -> {}
+# Kullanıcı çekmek istediği tutar hesabında varsa X yoksa Y işlemi.
+
+# 1 tab -> 1 indent içeri (sağ)
+# shift+tab -> 1 indent dışarı (sol)
+ogrenci = False
+if ogrenci == True:
+    print("Kisi ogrenci")
+else:
+    print("Kisi ogrenci degil")
+print("Blok disi")
+
+
+note = 75
+#elseif
+# Her koşul bloğu yalnızca 1 adet koşulu çalıştırır.
+if note >= 80:
+    print("AA")
+elif note >= 70: # bu koşul sağlanmıyor ama bu sağlanıyorsa
+    print("BB")
+elif note >= 60:
+    print("CC")
+elif note >= 50:
+    print("DD")
+else: # yukarıdaki koşulların hiç biri sağlanmıyorsa.
+    print("FF")
+
+
+# Eğer fatura tarihi geçmişse hesabı dondur.
+faturaOdenmedi = True
+onGunGecti = False
+if faturaOdenmedi:
+    print("Fatura ödenmedi işlemler yapılıyor..")
+    if onGunGecti:
+        print("Hesap donduruluyor.")
+    else:
+        print("Hatırlatma mesajı atılıyor.")
+
+
+# aynı scopedaki kodu N adet tekrarlayarak çalıştırma -> Döngüler
+
+# index => i
+# i=0 dan başlayan bir sayı, sen bunu 10'a kadar (dahil değil) artırarak her artırma sonrası
+# scope'u çalıştır.
+for i in range(10):
+    print(i) # 10 kere çalıştı.
+
+#

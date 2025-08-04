36d310d9c164778491c4fb73e40afc613894fb2f
halitklyc <halitenes.kalayci@gmail.com>
2025-08-04 10:53:36 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Değişkenler
+# Tanımlandıktan sonra programda kullanılacak isimlendirilmiş veriler.
+
+print(10) #statik
+
+price = 500 # değişken tanımıdır. 
+# 6. satırdan itibaren price ismine 500 değerini atadım.
+print(price)
+
+price = 600 #Tip-güvenli değil!
+print(price)
+#
+print(type(price)) 
+# integer => tam sayı
+# str,string => metinsel ifade
+# float,double,decimal => ondalıklı sayı
+# bool,boolean => true-false
+price2 = "Merhaba"
+price2 = 100
+price3 = 50 
+print(type(price2))
+print(type(price3))
+
+# Harf veya _ ile başlamalı
+# Sayı içerebilir ama sayı ile başlayamaz.
+# Büyük-küçük harf duyarlıdır
+# Türkçe karakter içermemelidir.
+name = "Halit"
+Name = "Halit"
+
+title = "Etiya Homepage"#......
+

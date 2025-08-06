868931370113f77769558eeff20ea423f1666e8f
halitklyc <halitenes.kalayci@gmail.com>
2025-08-06 14:36:13 +0300

    pom

--- project/tests/test_login.py
+++ project/tests/test_login.py
@@ -1,2 +1,18 @@
 # Yeteneklerin farklı 
 # veriler/sıralar ile çağırılarak testlerin yazılması.
+
+#class TestLogin():
+   # def test_valid_login(self):
+from pages.login_page import LoginPage
+
+def test_valid_login(driver):
+    login_page = LoginPage(driver)
+    login_page.load()
+    login_page.login("standard_user","secret_sauce")
+    assert driver.current_url == "https://www.saucedemo.com/v1/inventory.html"
+
+def test_invalid_login(driver):
+    login_page = LoginPage(driver)
+    login_page.load()
+    login_page.login("standard_user","secret_sauce123")
+    assert login_page.get_error_message() == "Epic sadface: Username and password do not match any user in this service"

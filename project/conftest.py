11e0c642adb5fe2ddf8fafd1a0d2bd409113c592
halitklyc <halitenes.kalayci@gmail.com>
2025-08-07 14:23:23 +0300

    .

--- project/conftest.py
+++ project/conftest.py
@@ -2,11 +2,21 @@
 import pytest
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
-
-@pytest.fixture # bütün test fonksiyonlarına enjekte edilecek bağımluluk.
+from utils.screenshot import take_screenshot
+@pytest.fixture # bütün test fonksiyonlarına enjekte edilecek bağımlılık.
 def driver():
     options = Options()
     #options.add_argument("--headless")
     driver = Chrome(options=options)
+    driver.maximize_window()
     yield driver # returnun multiple hali.
     driver.quit()
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item,call):
+    outcome = yield
+    report = outcome.get_result()
+    if report.when == "call":
+        driver = item.funcargs.get("driver", None)
+        if driver is not None:
+            take_screenshot(driver, item.name)
